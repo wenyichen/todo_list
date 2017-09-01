bd70a5fdd3334d5a3eb083da37ee8734fee67cbe
Wenyi Chen <wenyichen@outlook.com>
2017-09-01 15:59:00 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
