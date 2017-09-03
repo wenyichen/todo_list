23353e8213c20b2f770bdc2f7329553b9fbbcbc0
Wenyi Chen <wenyichen@outlook.com>
2017-09-02 22:15:11 -0700

    initialized server.js

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+
+var uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri);
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({'extended' : 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+app.listen(process.env.PORT);
+console.log('listening on *:' + process.env.PORT);
