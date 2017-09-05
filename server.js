8683b715e399b14048f6c6b3c8f08485d37f33c6
Wenyi Chen <wenyichen@outlook.com>
2017-09-05 07:00:26 -0700

    made some bugfix

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ app.delete('/api/items/:id', function(req, res) {
   });
 });
 
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+app.get('*', function(req, res) {
+  res.sendfile(path.resolve(__dirname, './client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3001);
